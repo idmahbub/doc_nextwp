0b3bf24638b49d3259e0252d41204c880b37ffb2
idmahbub <bubyc58@gmail.com>
2024-02-03 16:54:19 +0700

    initial commit nextwp doc

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>NextWp Doc</span>,
+  project: {
+    link: 'https://github.com/idmahbub/doc_nextwp',
+  },
+  chat: {
+    link: 'https://discordapp.com/users/idmahbub',
+  },
+  docsRepositoryBase: 'https://github.com/idmahbub/doc_nextwp',
+  footer: {
+    text: 'NextWp Doc',
+  },
+}
+
+export default config
