fd0c9ab6ca67d69f19370b0ccf00d891eaff9cad
idmahbub <bubyc58@gmail.com>
2024-02-08 03:08:23 +0700

    add toggles #2

--- pages/api/index.js
+++ pages/api/index.js
@@ -4,7 +4,7 @@ export const runtime = 'edge'
 export default async function GET(request) {
   const { searchParams } = new URL(request.url);
   const toggles = searchParams.get('toggles')
-  if (toggles==='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9'){
+  if (toggles===process.env.DEV_ACCESS){
     return NextResponse.json(
       {"data":{"amount":"19.84","sold_at":"2016-09-07T10:54:28+10:00","license":"Regular License","support_amount":"0.00","supported_until":"2017-03-09T01:54:28+11:00","item":{"id":17022701,"name":"SEO Studio - Professional Tools for SEO","description":"<strong>Full</strong> HTML description here...","author_username":"baileyherbert","updated_at":"2017-11-02T15:57:41+11:00","site":"codecanyon.net","price_cents":2000,"published_at":"2016-07-13T19:07:03+10:00"},"buyer":"buyer_username_or_null","purchase_count":1}}
     )
